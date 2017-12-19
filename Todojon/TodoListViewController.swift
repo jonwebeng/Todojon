356310878e540a24e485bdfcf3dd469d51150d0c
JON SKYNET <jonskynet@jons-macbook-pro.local>
2017-12-20 00:34:04 +0100

    Second Commit: Add a new Item

--- Todojon/TodoListViewController.swift
+++ Todojon/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-    let itemArray = [ "Find Touristologists", "Find Web-Engineeers", "Find Investors"]
+    var itemArray = [ "Find Touristologists", "Find Web-Engineeers", "Find Investors"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,8 +32,26 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    // MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todojon Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on our UIAlert
+            //print("Success!")
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            //print("alertTextField.text")
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
     
-
 
 }
 
